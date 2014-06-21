4f8e51f6a253b1f00418be41292983d0508070bc
thillux <theil.markus@gmail.com>
2014-06-21 10:59:25 +0200

    updated hamcrest

--- runTests.ps1
+++ runTests.ps1
@@ -1,7 +1,7 @@
 $TESTPATH="test"
 $SRCPATH="src"
 $LIBPATH="lib"
-$CLASSPATH="$LIBPATH\junit.jar;$TESTPATH;$SRCPATH"
+$CLASSPATH="$LIBPATH\junit.jar;$LIBPATH\hamcrest-all-1.3.jar;$TESTPATH;$SRCPATH"
 
 # run tests
 java -cp $CLASSPATH org.junit.runner.JUnitCore InputReaderTest FileUtilsTest
--- runTests.sh
+++ runTests.sh
@@ -4,6 +4,6 @@ TESTPATH=test
 SRCPATH=src
 LIBPATH=lib
 
-export CLASSPATH=$LIBPATH/junit.jar:$TESTPATH:$SRCPATH
+export CLASSPATH=$LIBPATH/junit.jar:$LIBPATH/hamcrest-all-1.3.jar:$TESTPATH:$SRCPATH
 
 java org.junit.runner.JUnitCore InputReaderTest FileUtilsTest
