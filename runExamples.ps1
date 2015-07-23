d1ec67034c2ce3214f86dc8f9c76295d5ea6d2ee
thillux <theil.markus@gmail.com>
2015-07-23 22:22:45 +0200

    updated junit and removed windows stuff

--- runExamples.ps1
+++
@@ -1,8 +0,0 @@
-$TESTPATH="test"
-$SRCPATH="src"
-$LIBPATH="lib"
-$EXAMPLEPATH="examples"
-$CLASSPATH="$LIBPATH\junit.jar;$TESTPATH;$SRCPATH;$EXAMPLEPATH"
-
-# run tests
-java -cp $CLASSPATH ReadNumbers
--- runTests.ps1
+++
@@ -1,7 +0,0 @@
-$TESTPATH="test"
-$SRCPATH="src"
-$LIBPATH="lib"
-$CLASSPATH="$LIBPATH\junit.jar;$LIBPATH\hamcrest-all-1.3.jar;$TESTPATH;$SRCPATH"
-
-# run tests
-java -cp $CLASSPATH org.junit.runner.JUnitCore InputReaderTest FileUtilsTest
